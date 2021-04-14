f593acf14c621db1c736424653a4913bda697930
Tommy Veen <75741894+tommyveen@users.noreply.github.com>
2021-04-14 22:15:47 +0200

    Add files via upload

---
+++ JavaScript_003/index.js
@@ -0,0 +1,20 @@
+const age = 19;
+if (age >= 18) {
+    console.log("Access approved. Please, enter the festival grounds!");
+} else {
+    console.log("Access denied, you are under 18. Please, leave the festival grounds.");
+}
+
+const isFemale = false;
+if (isFemale === true) {
+    console.log("Enjoy your stay!");
+} else {
+    console.log("It's ladies only night. Your access is denied.");
+}
+
+const driverStatus = 'bob';
+if (driverStatus === 'bob') {
+    console.log("Thank you for your visit. Your status: Bob. Drive safe!");
+} else {
+    console.log("Thank you for your visit. Your status: Alcoholic. You are not allowed to drive!");
+}
